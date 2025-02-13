b5bbd460a9ae68ea981f9839fc10b8bb6d7a9000
la la <13687739+daedalus1215@users.noreply.github.com>
2025-02-12 20:34:19 -0500

    init: fill in controller and stub service

--- src/apple-tv/apple-tv.controller.ts
+++ src/apple-tv/apple-tv.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AppleTvService } from './apple-tv.service';
 
 @Controller('apple-tv')
-export class AppleTvController {}
+export class AppleTvController {
+  constructor(private readonly appleTvService: AppleTvService) {}
+
+  @Post('up')
+  async up() {
+    return this.appleTvService.up();
+  }
+
+  @Post('down')
+  async down() {
+    return this.appleTvService.down();
+  }
+
+  @Post('left')
+  async left() {
+    return this.appleTvService.left();
+  }
+
+  @Post('right')
+  async right() {
+    return this.appleTvService.right();
+  }
+
+  @Post('select')
+  async select() {
+    return this.appleTvService.select();
+  }
+
+  @Post('menu')
+  async menu() {
+    return this.appleTvService.menu();
+  }
+}
--- src/apple-tv/apple-tv.service.ts
+++ src/apple-tv/apple-tv.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppleTvService {}
+export class AppleTvService {
+  async up() {
+    // Logic to send "up" command to Apple TV
+  }
+
+  async down() {
+    // Logic to send "down" command to Apple TV
+  }
+
+  async left() {
+    // Logic to send "left" command to Apple TV
+  }
+
+  async right() {
+    // Logic to send "right" command to Apple TV
+  }
+
+  async select() {
+    // Logic to send "select" command to Apple TV
+  }
+
+  async menu() {
+    // Logic to send "menu" command to Apple TV
+  }
+}
